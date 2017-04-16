804cd0e29a5ba46d4082e41a694d8ad399b7f117
seyvoue <neil.googl+github@gmail.com>
2017-04-16 17:10:56 +0800

    add: Android几个常用控件

--- UIWidgetTest/app/src/main/java/com/letica/demo/android/uiwidgettest/MainActivity.java
+++ UIWidgetTest/app/src/main/java/com/letica/demo/android/uiwidgettest/MainActivity.java
@@ -1,13 +1,74 @@
 package com.letica.demo.android.uiwidgettest;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private ImageView imageView;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        editText = (EditText) findViewById(R.id.edit_text);
+        imageView = (ImageView) findViewById(R.id.imag_view);
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                /* 实现通过点击button，显示editView中输入的内容，以Toast的方式 */
+//                String inputText = editText.getText().toString();
+//                Toast.makeText(MainActivity.this, inputText, Toast.LENGTH_SHORT).show();
+                /* 实现通过点击button，显示drawable中的另一张图片 */
+//                imageView.setImageResource(R.drawable.img_2);
+                /* 实现通过点击button，显示/不显示进度条控件progressBar */
+//                if(progressBar.getVisibility() == View.GONE){
+//                    progressBar.setVisibility(View.VISIBLE);
+//                }else{
+//                    progressBar.setVisibility(View.GONE);
+//                }
+                /* 实现通过点击button，动态地改变进度条的进度 */
+//                int progress = progressBar.getProgress();
+//                progress = progress + 10;
+//                progressBar.setProgress(progress);
+                /* 增加一个弹出对话框，以提示用户一些重要信息 */
+//                AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+//                dialog.setTitle("This is Dialog");
+//                dialog.setMessage("Something important.");
+//                dialog.setCancelable(false);
+//                dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//
+//                    }
+//                });
+//                dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//
+//                    }
+//                });
+//                dialog.show();
+                /* 增加一个带进度条的弹出对话框，以提示用户耐心等待 */
+                ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+                progressDialog.setTitle("This is ProgressDialog");
+                progressDialog.setMessage("Loading...");
+                progressDialog.setCancelable(true);
+                progressDialog.show();
+            }
+        });
     }
 }
