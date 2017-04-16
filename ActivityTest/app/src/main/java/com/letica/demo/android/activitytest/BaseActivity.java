cd700455c227487c6566c76c190d60360f0964da
seyvoue <neil.googl+github@gmail.com>
2017-04-16 14:17:25 +0800

    update: ActivityTest 完成

---
+++ ActivityTest/app/src/main/java/com/letica/demo/android/activitytest/BaseActivity.java
@@ -0,0 +1,27 @@
+package com.letica.demo.android.activitytest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+/**
+ * Created by Erxi on 4/15/2017.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+    private static final String TAG = "BaseActivity";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, getClass().getSimpleName());
+        ActivityCollector.addActivity(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ActivityCollector.removeActivity(this);
+    }
+}
