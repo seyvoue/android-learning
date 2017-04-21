8831bd406d160f176183d4806b9ceb3e0452a016
seyvoue <neil.googl+github@gmail.com>
2017-04-21 21:36:34 +0800

    update: FragmentBestPractice 以及 UIBestPractice 项目调通

---
+++ FragmentBestPractice/app/src/main/java/com/letica/demo/android/fragmentbestpractice/News.java
@@ -0,0 +1,22 @@
+package com.letica.demo.android.fragmentbestpractice;
+
+/**
+ * Created by Erxi on 4/21/2017.
+ */
+
+public class News {
+    private String title;
+    private String content;
+    public String getTitle(){
+        return title;
+    }
+    public void setTitle(String title){
+        this.title = title;
+    }
+    public String getContent(){
+        return content;
+    }
+    public void setContent(String content){
+        this.content = content;
+    }
+}
